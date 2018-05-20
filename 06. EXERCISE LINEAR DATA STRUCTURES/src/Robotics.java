abd8a6174561e3914e91633978470ebcd067f8e4
Bebrevski <stefan.bebrevski89@gmail.com>
2018-05-20 14:52:47 +0300

    Course Exercises

--- 06. EXERCISE LINEAR DATA STRUCTURES/src/Robotics.java
+++ 06. EXERCISE LINEAR DATA STRUCTURES/src/Robotics.java
@@ -1,5 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Robotics {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] robots = reader.readLine().split("[-;]+");
+        int[] start = Arrays.stream(reader.readLine().split(":"))
+                .mapToInt(Integer::parseInt).toArray();
+        int timeInSeconds = start[0] * 3600 + start[1] * 60 + start[2];
+
+        String product = reader.readLine();
+        while (!product.equals("END")) {
+
+            product = reader.readLine();
+        }
     }
 }
---
+++ 06. EXERCISE LINEAR DATA STRUCTURES/src/SimpleTextEditor.java
@@ -0,0 +1,5 @@
+public class SimpleTextEditor {
+    public static void main(String[] args) {
+        
+    }
+}
