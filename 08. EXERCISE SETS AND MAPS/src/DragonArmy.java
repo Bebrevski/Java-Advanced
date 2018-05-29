91eead9344ecf87d13acd469e6b91cab0d080acc
Bebrevski <stefan.bebrevski89@gmail.com>
2018-05-29 20:09:02 +0300

    Course exercises

--- 08. EXERCISE SETS AND MAPS/src/DragonArmy.java
+++ 08. EXERCISE SETS AND MAPS/src/DragonArmy.java
@@ -1,100 +1,86 @@
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DragonArmy {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static class DragonStats {
-        private Integer Damage;
-        private Integer Health;
-        private Integer Armor;
+        int countOfDragons = Integer.parseInt(reader.readLine());
 
-        DragonStats(Integer damage, Integer health, Integer armor) {
-            this.setDamage(damage);
-            this.setHealth(health);
-            this.setArmor(armor);
+        Map<String, Map<String, Dragon>> dragons = new LinkedHashMap<>();
+
+        for (int i = 0; i < countOfDragons; i++) {
+            String[] dragonTokens = reader.readLine().split("\\s+");
+
+            String type = dragonTokens[0];
+            String name = dragonTokens[1];
+
+            Integer damage = !dragonTokens[2].equals("null") ? Integer.parseInt(dragonTokens[2]) : 45;
+            Integer health = !dragonTokens[3].equals("null") ? Integer.parseInt(dragonTokens[3]) : 250;
+            Integer armor = !dragonTokens[4].equals("null") ? Integer.parseInt(dragonTokens[4]) : 10;
+
+            Dragon dragon = new Dragon(name, damage, health, armor);
+
+            Map<String, Dragon> dragonSkills = new LinkedHashMap<>();
+            if (dragons.containsKey(type)) {
+                dragonSkills = dragons.get(type);
+            }
+            dragonSkills.put(name, dragon);
+            dragons.put(type, dragonSkills);
         }
 
-        Integer getDamage() {
-            return this.Damage;
-        }
-
-        void setDamage(Integer newValue) {
-            this.Damage = newValue;
-        }
-
-        Integer getHealth() {
-            return this.Health;
-        }
-
-        void setHealth(Integer newValue) {
-            this.Health = newValue;
-        }
-
-        Integer getArmor() {
-            return this.Armor;
-        }
-
-        void setArmor(Integer newValue) {
-            this.Armor = newValue;
+        for (Map.Entry<String, Map<String, Dragon>> dragonEntry : dragons.entrySet()) {
+            List<Dragon> sortedDragons = dragonEntry
+                    .getValue()
+                    .values()
+                    .stream()
+                    .sorted(Comparator.comparing(Dragon::getName))
+                    .collect(Collectors.toList());
+            double averageDamage = sortedDragons.stream().mapToInt(Dragon::getDamage).average().getAsDouble();
+            double averageHealth = sortedDragons.stream().mapToInt(Dragon::getHealth).average().getAsDouble();
+            double averageArmor = sortedDragons.stream().mapToInt(Dragon::getArmor).average().getAsDouble();
+            System.out.println(String.format("%s::(%.2f/%.2f/%.2f)", dragonEntry.getKey(), averageDamage, averageHealth, averageArmor));
+            for (Dragon dragon : sortedDragons) {
+                System.out.println(dragon);
+            }
         }
     }
+}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+class Dragon {
+    private String name;
+    private int damage;
+    private int health;
+    private int armor;
 
-        Map<String, Map<String, DragonStats>> record = new LinkedHashMap<>();
+    Dragon(String name, int damage, int health, int armor) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.armor = armor;
+    }
 
-        Integer n = Integer.parseInt(scanner.nextLine());
-        for (int i = 0; i < n; i++) {
-            String[] tokens = scanner.nextLine().split(" ");
+    String getName() {
+        return this.name;
+    }
 
-            String type = tokens[0];
-            String name = tokens[1];
+    int getDamage() {
+        return this.damage;
+    }
 
-            Integer damage = !tokens[2].equals("null") ? Integer.parseInt(tokens[2]) : 45;
-            Integer health = !tokens[3].equals("null") ? Integer.parseInt(tokens[3]) : 250;
-            Integer armor = !tokens[4].equals("null") ? Integer.parseInt(tokens[4]) : 10;
+    int getHealth() {
+        return this.health;
+    }
 
-            record.putIfAbsent(type, new LinkedHashMap<>());
-            DragonStats currentDragon = new DragonStats(damage, health, armor);
-            record.get(type).putIfAbsent(name, currentDragon);
-        }
+    int getArmor() {
+        return this.armor;
+    }
 
-        Double averageDamage = 0D;
-        Double averageHealth = 0D;
-        Double averageArmor = 0D;
-        for (Map.Entry<String, Map<String, DragonStats>> dragon : record.entrySet()) {
-            for (Map.Entry<String, DragonStats> kvp : dragon.getValue().entrySet()) {
-                averageDamage += kvp.getValue().getDamage();
-                averageHealth += kvp.getValue().getHealth();
-                averageArmor += kvp.getValue().getArmor();
-            }
-
-            Integer count = dragon.getValue().size();
-            averageDamage /= count;
-            averageHealth /= count;
-            averageArmor /= count;
-
-            System.out.printf("%s::(%.2f/%.2f/%.2f)%n"
-                    , dragon.getKey()
-                    , averageDamage
-                    , averageHealth
-                    , averageArmor);
-
-            dragon.getValue().entrySet()
-                    .stream()
-                    .sorted(Comparator.comparing(Map.Entry::getKey))
-                    .forEach(d -> System.out.printf("-%s -> damage: %d, health: %d, armor: %d%n"
-                    ,d.getKey()
-                    ,d.getValue().getDamage()
-                    ,d.getValue().getHealth()
-                    ,d.getValue().getArmor()));
-
-            averageDamage = 0D;
-            averageHealth = 0D;
-            averageArmor = 0D;
-        }
+    @Override
+    public String toString() {
+        return String.format("-%s -> damage: %d, health: %d, armor: %d", this.name, this.damage, this.health, this.armor);
     }
 }
