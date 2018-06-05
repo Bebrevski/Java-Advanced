49f01f8d5bc1a3320c77c7df7b8fa2994c40460c
Bebrevski <stefan.bebrevski89@gmail.com>
2018-06-05 22:28:34 +0300

    Course exercises

--- 16. EXAM PREPS/1. Java Fundamentals Exam - 19 February 2017/src/Earthquake.java
+++ 16. EXAM PREPS/1. Java Fundamentals Exam - 19 February 2017/src/Earthquake.java
@@ -15,47 +15,36 @@ public class Earthquake {
         ArrayDeque<List<Integer>> queue = new ArrayDeque<>();
 
         for (int i = 0; i < count; i++) {
-            List<Integer> input = Arrays.stream(reader.readLine().split(" "))
-                    .map(Integer::parseInt).collect(Collectors.toList());
-
-            queue.offer(input);
+            queue.offer(Arrays.stream(reader.readLine().split("\\s+"))
+                    .map(Integer::parseInt).collect(Collectors.toList()));
         }
 
         ArrayDeque<Integer> outputQueue = new ArrayDeque<>();
 
         while (!queue.isEmpty()) {
-
-            if (queue.peek().size() == 1) {
-                outputQueue.offer(queue.poll().get(0));
-                continue;
-            }
-
             List<Integer> activity = queue.poll();
 
+            int seismicity = activity.get(0);
+
             for (int i = 1; i < activity.size(); i++) {
-                int seismicity = activity.get(0);
                 int wave = activity.get(i);
 
-                if (seismicity >= wave) {
-                    activity.remove(i);
-                    i--;
-
-                    if (activity.size() == 1) {
-                        queue.offer(activity);
-                    }
-
-                } else {
-                    activity.remove(0);
-                    outputQueue.offer(seismicity);
-                    queue.offer(activity);
-                    break;
+                if (seismicity < wave) {
+                  queue.offer(activity.subList(i, activity.size()));
+                  break;
                 }
             }
+
+            outputQueue.offer(seismicity);
         }
 
         System.out.println(outputQueue.size());
+        StringBuilder sb = new StringBuilder();
         for (Integer num : outputQueue) {
-            System.out.print(num + " ");
+            sb.append(num).append(" ");
         }
+        System.out.println(sb);
     }
 }
+
+
